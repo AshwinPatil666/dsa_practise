5ac09a83fc28963fb4bf470c52d58af7d8495ed1
AshwinPatil666 <ashwinpatil2050@gmail.com>
2026-08-19 17:20:11 +0530

    first commit

---
+++ hashing/dimondpiramid.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i=1;i<=n+1;i++){
+      
+    for(int j=1;j<=(n+1)-i;j++){
+      
+      cout<<"  ";
+    }
+
+    for(int b=1;b<=2*i-1;b++){
+      
+
+      cout<<"* ";
+      
+    }
+cout<<"\n";
+    }
+    for(int i=1;i<=n+1;i++){
+    for(int j=1;j<=i-1;j++){
+      
+      cout<<"  ";
+    }
+
+    for(int b=n*2+1;b>=2*i-1;b--){
+      
+
+      cout<<"* ";
+      
+    }
+    cout<<"\n";
+
+
+}
+}
